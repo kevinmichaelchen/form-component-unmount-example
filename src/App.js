05f71c4a212f04ed5a74c55a13c82dae489156e3
Kevin Chen <kevinmichaelchen@gmail.com>
2020-09-28 21:36:47 -0400

    Reproduced 'state update on unmounted component' error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { useForm } from "react-hook-form";
+import {
+  Redirect,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+const Page1 = () => {
+  const { handleSubmit, register } = useForm();
+  const [formData, setFormData] = useState(undefined);
+  const [isLoading, setLoading] = useState(undefined);
+  const [error, setError] = useState(undefined);
+  const { user, setUser, setSession } = React.useContext(AuthContext);
+
+  const onSubmit = async (data) => {
+    console.log("data", data);
+    setFormData(data);
+  };
+  useEffect(() => {
+    function timeout(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    async function login() {
+      if (formData) {
+        setLoading(true);
+        await timeout(1000);
+        setSession({ id: 1 });
+        await timeout(1000);
+        setUser({ name: "Kevin" });
+        setLoading(false);
+      }
+    }
+
+    login();
+  }, [setSession, setUser, formData]);
+
+  if (user) {
+    return <Redirect to={"/2"} />;
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="name" ref={register} />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+const Page2 = () => {
+  return <div>Page 2</div>;
+};
+
+export const AuthContext = React.createContext();
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const [session, setSession] = useState(undefined);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={{ user, setUser, session, setSession }}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Page1} />
+            <Route path="/2" exact component={Page2} />
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
